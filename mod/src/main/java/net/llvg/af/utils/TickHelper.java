7e5eaecc7b86e53a50ed5655adf54a0d3208f1f2
Water-OR <a12300680@126.com>
2025-07-14 13:34:14 +0800

    remove unused TickHelper

--- mod/src/main/java/net/llvg/af/utils/TickHelper.java
+++
@@ -1,9 +0,0 @@
-package net.llvg.af.utils;
-
-public final class TickHelper {
-    private TickHelper() {
-        throw new UnsupportedOperationException();
-    }
-    
-    private static long ticks = 0;
-}
